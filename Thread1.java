efb438fb70763c7dc927a841ce18d0869ffd8e7c
David Fielder <thepyro395@gmail.com>
2017-04-25 22:05:30 -0500

    Update Thread1.java

--- Thread1.java
+++ Thread1.java
@@ -1,13 +1,16 @@
-public class Thread1 extends Thread{
+public class Thread1 extends Thread
+{
 	   //private String name;
 	   private int[] list;
 		
-		public Thread1(String name, int[] list){
+		public Thread1(String name, int[] list)
+		{
 			this.list=list;
 		}
 		
 		@Override
-		public void run() {
+		public void run() 
+		{
 			 int index;
 			 long start;
 			 start = System.currentTimeMillis();
