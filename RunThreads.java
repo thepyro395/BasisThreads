2074f2cfcb97302a2a8c83ef57de3e41be4e94c8
David Fielder <thepyro395@gmail.com>
2017-04-25 22:05:09 -0500

    Create RunThreads.java

---
+++ RunThreads.java
@@ -0,0 +1,25 @@
+public class RunThreads 
+{
+
+	public static void main(String[] args) 
+  {
+		int[] list = new int[3000000];
+		long endTime;
+		//Run Thread that implements Runnable Interface
+		Thread1 task1 = new Thread1("First Thread", list);
+		task1.start();
+		
+		
+		//Run Thread that inherits from Thread Class
+		Thread2 task2 = new Thread2("Second Thread", list);
+		task2.start();
+
+		Thread3 task3 = new Thread3("First Thread", list);
+		task3.start();
+		
+		endTime = System.currentTimeMillis();
+		System.out.println(endTime);
+	
+	}
+
+}
