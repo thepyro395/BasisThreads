a6f465c2db27017d39318358a1f51b17f210b10b
David Fielder <thepyro395@gmail.com>
2017-04-25 22:01:33 -0500

    Create Thread2.java

---
+++ Thread2.java
@@ -0,0 +1,24 @@
+public class Thread2 extends Thread{
+	   //private String name;
+	   private int[] list;
+		
+		public Thread2(String name, int[] list){
+			//this.name=name;
+			this.list = list;
+		}
+		
+		@Override
+		public void run() {
+			long start;
+			start = System.currentTimeMillis();
+			
+		   int index;
+		   for(index = 1000000; index<2000000;index ++)
+		   {
+		  	 list[index] = list[index] + 100;
+		   }
+			long end = System.currentTimeMillis();
+			long elaspedTime = end-start;
+			System.out.println("Elapsed Time = " + elaspedTime);
+		}
+}
