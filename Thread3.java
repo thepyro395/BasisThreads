18243adffd34cea849ac3738ee1faf6e3696d85a
David Fielder <thepyro395@gmail.com>
2017-04-25 22:02:03 -0500

    Create Thread3.java

---
+++ Thread3.java
@@ -0,0 +1,23 @@
+public class Thread3 extends Thread{
+	  // private String name;
+	   private int[] list;
+		
+		public Thread3(String name, int[] list){
+			this.list=list;
+		}
+		
+		@Override
+		public void run() {
+			 int index;
+			 long start;
+			 start = System.currentTimeMillis();
+		   for(index = 2000000; index<3000000;index ++)
+		   {
+		  	 list[index] = list[index] + 100;
+		   }
+		   long end = System.currentTimeMillis();
+				long elaspedTime = end-start;
+				System.out.println("Elapsed Time = " + elaspedTime);
+			
+		}
+}
